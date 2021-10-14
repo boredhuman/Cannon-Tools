7a391dbccadfa237a80f265364bdf02000905326
Daniel <55595431+boredhuman@users.noreply.github.com>
2021-10-14 21:02:37 +0100

    Stuff can't remember

--- src/main/java/dev/boredhuman/cannontools/CannonToolsMod.java
+++ src/main/java/dev/boredhuman/cannontools/CannonToolsMod.java
@@ -1,6 +1,7 @@
 package dev.boredhuman.cannontools;
 
 import dev.boredhuman.api.BHCoreAPI;
+import dev.boredhuman.api.config.CategoryStart;
 import dev.boredhuman.api.config.ConfigMinMax;
 import dev.boredhuman.api.config.ConfigProperty;
 import dev.boredhuman.api.events.BatchedLineRenderingEvent;
@@ -9,6 +10,7 @@ import dev.boredhuman.api.events.RenderHook;
 import dev.boredhuman.api.module.AbstractModule;
 import dev.boredhuman.api.module.ModColor;
 import dev.boredhuman.api.module.Module;
+import dev.boredhuman.api.rendering.shapes.Box;
 import dev.boredhuman.api.util.MutableValue;
 import dev.boredhuman.gui.util.Colors;
 import dev.boredhuman.util.Pair;
@@ -23,11 +25,11 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -36,14 +38,14 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Module(name = "Cannon Tools", author = "BoredHuman", description = "Tools to help cannon development.")
-@Mod(modid = "Cannon Tools", dependencies = "after:BHCore")
+@Mod(modid = "cannontools")
 public class CannonToolsMod extends AbstractModule {
 
-	private Map<Integer, Pair<LinkedList<Long>, LinkedList<Position>>> TNTLines = new ConcurrentHashMap<>();
-	private Map<Integer, Pair<LinkedList<Long>, LinkedList<Position>>> fallingBlockLines = new ConcurrentHashMap<>();
-	private Map<Position, Long> tntPositionTime = new ConcurrentHashMap<>();
-	private Set<Pair<Position, Integer>> liveTNT = new HashSet<>();
-	private Set<Position> liveSand = new HashSet<>();
+	private final Map<Integer, Pair<LinkedList<Long>, LinkedList<Position>>> TNTLines = new ConcurrentHashMap<>();
+	private final Map<Integer, Pair<LinkedList<Long>, LinkedList<Position>>> fallingBlockLines = new ConcurrentHashMap<>();
+	private final Map<Position, Long> tntPositionTime = new ConcurrentHashMap<>();
+	private final Set<Pair<Position, Integer>> liveTNT = new HashSet<>();
+	private final Set<Position> liveSand = new HashSet<>();
 
 	@ConfigProperty(name = "Clear Time /s")
 	@ConfigMinMax(min = 1, max = 30)
@@ -53,11 +55,9 @@ public class CannonToolsMod extends AbstractModule {
 	@ConfigMinMax(min = 1, max = 5)
 	private MutableValue<Float> lineWidth = new MutableValue<>(2F);
 
-	@ConfigProperty(name = "TNT Line Color")
-	private ModColor tntLineColor = new ModColor(Colors.RED.getIntColor());
-
-	@ConfigProperty(name = "Sand Line Color")
-	private ModColor fallingBlockLineColor = new ModColor(Colors.YELLOW.getIntColor());
+	@CategoryStart(name = "Explosion Box Settings")
+	@ConfigProperty(name = "Explosion Box")
+	private MutableValue<Boolean> explosionBox = new MutableValue<>(false);
 
 	@ConfigProperty(name = "Box Color")
 	private ModColor explosionBoxColor = new ModColor(Colors.RED.getIntColor());
@@ -70,39 +70,76 @@ public class CannonToolsMod extends AbstractModule {
 	@ConfigMinMax(min = 1, max = 5)
 	private MutableValue<Float> boxLineWidth = new MutableValue<>(2F);
 
+	@CategoryStart(name = "TNT Settings")
 	@ConfigProperty(name = "TNT Breadcrumbs")
 	private MutableValue<Boolean> showTNTCrumbs = new MutableValue<>(true);
 
+	@ConfigProperty(name = "TNT Line Color")
+	private ModColor tntLineColor = new ModColor(Colors.RED.getIntColor());
+
+	@ConfigProperty(name = "Minimal TNT")
+	private MutableValue<Boolean> minimalTNT = new MutableValue<>(false);
+
+	@ConfigProperty(name = "TNT Triangle Lines")
+	private MutableValue<Boolean> tntTriangles = new MutableValue<>(false);
+
+	@CategoryStart(name = "Sand Settings")
 	@ConfigProperty(name = "Sand Breadcrumbs")
 	private MutableValue<Boolean> showSandCrumbs = new MutableValue<>(true);
 
+	@ConfigProperty(name = "Sand Line Color")
+	private ModColor fallingBlockLineColor = new ModColor(Colors.YELLOW.getIntColor());
+
+	@ConfigProperty(name = "Minimal Sand")
+	private MutableValue<Boolean> minimalSand = new MutableValue<>(false);
+
+	@ConfigProperty(name = "Sand Triangle Lines")
+	private MutableValue<Boolean> sandTriangles = new MutableValue<>(false);
+
+	@CategoryStart(name = "Rendering Settings")
 	@ConfigProperty(name = "TNT Flashing")
 	public MutableValue<Boolean> doTNTFlashing = new MutableValue<>(true);
 
 	@ConfigProperty(name = "TNT Expand")
 	public MutableValue<Boolean> doTNTExpand = new MutableValue<>(true);
 
-	@ConfigProperty(name = "Minimal TNT")
-	private MutableValue<Boolean> minimalTNT = new MutableValue<>(false);
-
-	@ConfigProperty(name = "Minimal Sand")
-	private MutableValue<Boolean> minimalSand = new MutableValue<>(false);
-
 	@ConfigProperty(name = "Do Depth")
 	private MutableValue<Boolean> doDepth = new MutableValue<>(false);
 
-	@Mod.Instance
+	@CategoryStart(name = "Patch Crumbs Settings")
+	@ConfigProperty(name = "Patch Crumbs")
+	private MutableValue<Boolean> patchCrumbs = new MutableValue<>(false);
+
+	@ConfigProperty(name = "Patch Color")
+	private ModColor patchColor = new ModColor(0xFF00FF00);
+
+	@ConfigProperty(name = "Cube Thickness")
+	@ConfigMinMax(min = 0.1F, max = 0.5F)
+	private MutableValue<Float> cubeThickness = new MutableValue<>(0.25F);
+
+	RenderHook renderHook = phase -> {
+		if (!this.doDepth.getValue()) {
+			if (phase == RenderHook.Phase.SETUP) {
+				GlStateManager.disableDepth();
+			} else {
+				GlStateManager.enableDepth();
+			}
+		}
+	};
+
 	private static CannonToolsMod INSTANCE;
 
 	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		BHCoreAPI.getAPI().registerModules(this);
-		MinecraftForge.EVENT_BUS.register(this);
+	public void preInit(FMLPostInitializationEvent event) {
+		this.init();
 	}
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
+	@Override
+	public void init() {
+		CannonToolsMod.INSTANCE = this;
+		BHCoreAPI.getAPI().registerModules(this);
 		RenderingRegistry.registerEntityRenderingHandler(EntityTNTPrimed.class, new TNTRenderer(Minecraft.getMinecraft().getRenderManager()));
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
@@ -123,7 +160,12 @@ public class CannonToolsMod extends AbstractModule {
 		long now = System.currentTimeMillis();
 		for (Entity entity : mc.theWorld.loadedEntityList) {
 			Position position = new Position(entity.posX, entity.posY, entity.posZ);
-			if (entity instanceof EntityTNTPrimed && this.minimalTNT.getValue()) {
+			boolean isTNT = entity instanceof EntityTNTPrimed;
+			boolean isSand = entity instanceof EntityFallingBlock;
+			if (!isSand && !isTNT) {
+				continue;
+			}
+			if (isTNT && this.minimalTNT.getValue()) {
 				EntityTNTPrimed tnt = (EntityTNTPrimed) entity;
 				Pair<Position, Integer> positionFusePair = new Pair<>(position, tnt.fuse);
 				if (this.liveTNT.contains(positionFusePair)) {
@@ -132,14 +174,16 @@ public class CannonToolsMod extends AbstractModule {
 				}
 				this.liveTNT.add(positionFusePair);
 			}
-			if (entity instanceof EntityFallingBlock && this.minimalSand.getValue()) {
+			if (isSand && this.minimalSand.getValue()) {
 				if (this.liveSand.contains(position)) {
 					entity.setDead();
 					continue;
 				}
 				this.liveSand.add(position);
 			}
-			if ((!(entity instanceof EntityTNTPrimed) || !this.showTNTCrumbs.getValue()) && (!(entity instanceof EntityFallingBlock)) || !this.showSandCrumbs.getValue()) {
+			boolean gatherTNTLines = isTNT && this.showTNTCrumbs.getValue();
+			boolean gatherSandLines = isSand && this.showSandCrumbs.getValue();
+			if ((!gatherTNTLines && !gatherSandLines && !this.patchCrumbs.getValue())) {
 				continue;
 			}
 			int id = entity.getEntityId();
@@ -151,11 +195,19 @@ public class CannonToolsMod extends AbstractModule {
 			}
 			Position last = listLinkedListPair.second.peekLast();
 			if (last == null || !last.equals(position)) {
+				boolean triangle = ((isTNT && this.tntTriangles.getValue()) || (isSand && this.sandTriangles.getValue())) && last != null;
+				if (triangle) {
+					listLinkedListPair.second.add(new Position(last.x, position.y, last.z));
+					listLinkedListPair.first.add(now);
+					listLinkedListPair.second.add(new Position(position.x, position.y, last.z));
+					listLinkedListPair.first.add(now);
+				}
 				listLinkedListPair.second.add(position);
 				listLinkedListPair.first.add(now);
 			}
 		}
 		this.liveTNT.clear();
+		this.liveSand.clear();
 	}
 
 	public void cleanUp(Map<Integer, Pair<LinkedList<Long>, LinkedList<Position>>> map) {
@@ -181,38 +233,90 @@ public class CannonToolsMod extends AbstractModule {
 
 	@SubscribeEvent
 	public void batchedLineStripEvent(BatchedLineRenderingEvent event) {
-		RenderHook renderHook = phase -> {
-			if (!this.doDepth.getValue()) {
-				if (phase == RenderHook.Phase.SETUP) {
-					GlStateManager.disableDepth();
-				} else {
-					GlStateManager.enableDepth();
-				}
+		if (this.showTNTCrumbs.getValue()) {
+			for (Pair<LinkedList<Long>, LinkedList<Position>> toDraw : this.TNTLines.values()) {
+				event.addLineStripLineWidth(Pair.of(this.lineWidth.getValue(), this.renderHook), new BatchedLineRenderingEvent.LineStrip(toDraw.second, this.tntLineColor.getBGRA()));
 			}
-		};
-		for (Pair<LinkedList<Long>, LinkedList<Position>> toDraw : this.TNTLines.values()) {
-			event.addLineStripLineWidth(Pair.of(this.lineWidth.getValue(), renderHook), new BatchedLineRenderingEvent.LineStrip(toDraw.second, this.tntLineColor.getGBRA()));
 		}
-		for (Pair<LinkedList<Long>, LinkedList<Position>> toDraw : this.fallingBlockLines.values()) {
-			event.addLineStripLineWidth(Pair.of(this.lineWidth.getValue(), renderHook), new BatchedLineRenderingEvent.LineStrip(toDraw.second, this.fallingBlockLineColor.getGBRA()));
-		}
-
-		List<Pair<Position, Position>> lines = new ArrayList<>();
-
-		for (Position position : this.tntPositionTime.keySet()) {
-			lines.addAll(this.makeOutline(position, this.explosionBoxRadius.getValue()));
+		if (this.showSandCrumbs.getValue()) {
+			for (Pair<LinkedList<Long>, LinkedList<Position>> toDraw : this.fallingBlockLines.values()) {
+				event.addLineStripLineWidth(Pair.of(this.lineWidth.getValue(), this.renderHook), new BatchedLineRenderingEvent.LineStrip(toDraw.second, this.fallingBlockLineColor.getBGRA()));
+			}
 		}
 
-		event.addLineLineWidth(Pair.of(this.boxLineWidth.getValue(), renderHook), new BatchedLineRenderingEvent.Line(lines, this.explosionBoxColor.getGBRA()));
+		if (this.explosionBox.getValue()) {
+			List<Pair<Position, Position>> lines = new ArrayList<>();
+
+			for (Position position : this.tntPositionTime.keySet()) {
+				lines.addAll(this.makeOutline(position, this.explosionBoxRadius.getValue()));
+			}
+
+			event.addLineLineWidth(Pair.of(this.boxLineWidth.getValue(), this.renderHook), new BatchedLineRenderingEvent.Line(lines, this.explosionBoxColor.getBGRA()));
+		}
+
+		if (this.patchCrumbs.getValue()) {
+			this.doPatchCrumbs(event);
+		}
 	}
 
 	@SubscribeEvent
 	public void onPacket(PacketEvent event) {
+		if (!this.explosionBox.getValue()) {
+			return;
+		}
 		if (event.getPacket() instanceof S27PacketExplosion) {
 			S27PacketExplosion explosion = (S27PacketExplosion) event.getPacket();
 			Position pos = new Position(explosion.getX(), explosion.getY(), explosion.getZ());
 			this.tntPositionTime.put(pos, System.currentTimeMillis());
 		}
+	}
+
+	public void doPatchCrumbs(BatchedLineRenderingEvent event) {
+		List<Position> candidates = new ArrayList<>();
+		for (Map.Entry<Integer, Pair<LinkedList<Long>, LinkedList<Position>>> entry : this.TNTLines.entrySet().toArray(new Map.Entry[0])) {
+			LinkedList<Position> lines = entry.getValue().second;
+			Position prevPosition = null;
+			for (Position line : lines) {
+				if (prevPosition == null) {
+					prevPosition = line;
+				}
+				if (prevPosition.y > line.y) {
+					break;
+				}
+				if (!prevPosition.equals(line)) {
+					candidates.add(line);
+					break;
+				}
+			}
+		}
+
+		Position highestPosition = null;
+		for (Position pos : candidates) {
+			if (highestPosition == null) {
+				highestPosition = pos;
+			}
+			if (pos.y > highestPosition.y) {
+				highestPosition = pos;
+			}
+		}
+
+		if (highestPosition == null) {
+			return;
+		}
+
+		float rad = this.cubeThickness.getValue();
+
+		Box patchXBox = new Box(highestPosition.x - 100, highestPosition.y - rad, highestPosition.z - rad, highestPosition.x + 100, highestPosition.y + rad, highestPosition.z + rad);
+		Box patchZBox = new Box(highestPosition.x - rad, highestPosition.y - rad, highestPosition.z - 100, highestPosition.x + rad, highestPosition.y + rad, highestPosition.z + 100);
+
+		List<Pair<Position, Position>> boxOutlines = new ArrayList<>();
+		boxOutlines.addAll(this.makeOutline(patchXBox.minX, patchXBox.minY, patchXBox.minZ, patchXBox.maxX, patchXBox.maxY, patchXBox.maxZ));
+		boxOutlines.addAll(this.makeOutline(patchZBox.minX, patchZBox.minY, patchZBox.minZ, patchZBox.maxX, patchZBox.maxY, patchZBox.maxZ));
+
+		event.addLineLineWidth(Pair.of(this.boxLineWidth.getValue(), this.renderHook), new BatchedLineRenderingEvent.Line(boxOutlines, this.patchColor.getBGRA()));
+
+		int color = this.patchColor.getBGRA();
+		event.addBoxesColor(color, Arrays.asList(new Box[]{patchXBox, patchZBox}), null);
 	}
 
 	public List<Pair<Position, Position>> makeOutline(Position position, double radius) {
@@ -223,6 +327,12 @@ public class CannonToolsMod extends AbstractModule {
 		double maxX = position.x + radius;
 		double maxY = position.y + radius;
 		double maxZ = position.z + radius;
+
+		return this.makeOutline(minX, minY, minZ, maxX, maxY, maxZ);
+	}
+
+	public List<Pair<Position, Position>> makeOutline(double minX, double minY, double minZ, double maxX, double maxY, double maxZ) {
+		List<Pair<Position, Position>> linesList = new ArrayList<>();
 		Position origin = new Position(minX, minY, minZ);
 		Position max = new Position(maxX, maxY, maxZ);
 		Position originX = new Position(maxX, minY, minZ);
@@ -252,5 +362,4 @@ public class CannonToolsMod extends AbstractModule {
 	public static CannonToolsMod getInstance() {
 		return CannonToolsMod.INSTANCE;
 	}
-
 }
